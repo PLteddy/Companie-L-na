688604c8a3c61764347f6f4c2708219f20b51f50
PLteddy <plt.sebdaoui@gmail.com>
2025-01-07 21:27:53 +0100

    5th commit.2

--- templates/contact.php
+++ templates/contact.php
@@ -15,8 +15,8 @@
         <hr>
         <h2>Image de test</h2>
         <?php $image = get_field( selector :'image'); ?>
-        <img src="<?php echo ($image ['sizes'['thumbnail']]);?>" alt='Image'> 
-        <pre><?php var_dump($image); ?> </pre>
+        <img src="<?php echo ($image ['sizes']['custom_size']);?>" height="<?php echo ($image ['sizes']['custom_size']);?>" width="<?php echo ($image ['sizes']['custom_size']);?>" alt='<?php echo ($image ['alt']);?>'> 
+        <pre><?php var_dump($image); ?> </pre> 
 
      </section>
    </div>
